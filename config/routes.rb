c9e41ea7c7c152deecf3db4856b957f0d926606e
Nikhil Goel <ngoel36@gmail.com>
2012-12-22 17:02:57 -0600

    route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Launchpage::Application.routes.draw do
   root :to => "users#new"
   match 'success' => 'static_pages#success'
   resources :users
-  
+  match 'blog' => 'http://bitcastio.tumblr.com'
 end
