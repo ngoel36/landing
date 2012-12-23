f6db8b4561975ead296c401f92b9673801afa7cd
Nikhil Goel <ngoel36@gmail.com>
2012-12-23 00:43:45 -0600

    fix links

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Launchpage::Application.routes.draw do
   root :to => "users#new"
   match 'success' => 'static_pages#success'
   resources :users
-  match 'blog' => 'http://bitcastio.tumblr.com'
 end
