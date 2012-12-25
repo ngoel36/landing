299cb7700959300a5d47a44d00d8b2ce37823a15
Nikhil Goel <ngoel36@gmail.com>
2012-12-25 16:20:49 -0600

    active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Launchpage::Application.routes.draw do  
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   root :to => "users#new"
   match 'success' => 'static_pages#success'
   resources :users
