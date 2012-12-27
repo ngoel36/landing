5217ae1a64274a3fe747def87fd619296b8e4d60
Zach Sherman <zksherm@gmail.com>
2012-12-27 01:00:42 -0600

    jquery fix and minor stuff

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,13 @@
 $(function(){
+	$('input:submit').bind('click', function(){
+		//console.log('submitting');
+		$('input:submit').attr('value','Submitting...');
+		//$('input:submit').attr('disabled', true);
+		$('.email-failure-message').text('');
+	});
 	$('.email-form form').bind('ajax:success', function(data, status, xhr){
 		//alert("success");
+		//console.log("success");
 		$('#user_email').css({border: '1px solid green'});
 		$('#user_email').remove();
 		$('input:submit').remove();
@@ -9,18 +16,12 @@ $(function(){
 	})
 	.bind('ajax:error', function(xhr, status, error){
 		//alert("error");
+		//console.log('error');
 		$('input:submit').removeAttr('disabled');
 		$('input:submit').attr('value','Request Invite');
 		$('#email-container').effect("shake", { times:3, distance:20 }, 500);
 		$('#user_email').css({border: '1px solid red'}).focus();
 		$('.email-failure-message').text('Sorry, that email has already been registered or is invalid.');
-	});
-	$('input:submit').bind('click', function(){
-		setTimeout(function(){
-			$('input:submit').attr('value','Submitting...');
-			$('input:submit').attr('disabled', true);
-			$('.email-failure-message').text('');
-		}, 10);
 	});
 
 }); 
